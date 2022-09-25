c6447a1d137192ce779dac3293cfb9c3537b353b
Donat <donat_kolzenov@mail.ru>
2022-09-26 01:38:11 +0700

    update: koch snowflake model

--- samples/KochSnowflake/Models/KochSnowflakeModel.cs
+++ samples/KochSnowflake/Models/KochSnowflakeModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace KochSnowflake.Models
 {
     using System;
+    using System.Collections.Generic;
+
+    using KochSnowflake.Models;
 
     public class KochSnowflakeModel
     {
+        public List<PointModel> Points { get; set; } = null!;
     }
 }
