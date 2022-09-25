12ecdad770d68f657e11d2dd519895985b570948
Donat <donat_kolzenov@mail.ru>
2022-09-26 01:29:54 +0700

    add: point model

---
+++ samples/KochSnowflake/Models/PointModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KochSnowflake.Models
+{
+    using System;
+
+    public class PointModel
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
