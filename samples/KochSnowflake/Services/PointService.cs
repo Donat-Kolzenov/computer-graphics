2f3150e52b7edb8465280c6f8e05c6446622e7c9
Donat <donat_kolzenov@mail.ru>
2022-09-26 01:35:42 +0700

    add: point service

---
+++ samples/KochSnowflake/Services/PointService.cs
@@ -0,0 +1,18 @@
+﻿namespace KochSnowflake.Services
+{
+    using System;
+
+    using KochSnowflake.Models;
+
+    public class PointService
+    {
+        public PointModel GeneratePoint(int x, int y)
+        {
+            return new PointModel()
+            {
+                X = x,
+                Y = y
+            };
+        }
+    }
+}
