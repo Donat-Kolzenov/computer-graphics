5a230df3c0fbc675d3393749e17dc3cd03efeb19
Donat <donat_kolzenov@mail.ru>
2022-10-11 01:49:42 +0700

    remove: ctor with x/y parameters

--- samples/DesktopSamples.Models/Point/PointModel.cs
+++ samples/DesktopSamples.Models/Point/PointModel.cs
@@ -2,12 +2,6 @@
 {
     public class PointModel
     {
-        public PointModel(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-
         public double X { get; set; }
 
         public double Y { get; set; }
