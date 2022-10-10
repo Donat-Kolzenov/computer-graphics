1848bf29913fe113b93a3c9030da28283cb51921
Donat <donat_kolzenov@mail.ru>
2022-10-11 01:50:46 +0700

    add: point service

---
+++ samples/DesktopSamples.Services/Point/PointService.cs
@@ -0,0 +1,17 @@
+﻿namespace DesktopSamples.Services.Point
+{
+    using DesktopSamples.Services.Interfaces;
+    using DesktopSamples.Models.Point;
+
+    public class PointService : IPointService
+    {
+        public PointModel GeneratePoint(double x, double y)
+        {
+            return new PointModel()
+            {
+                X = x,
+                Y = y
+            };
+        }
+    }
+}
