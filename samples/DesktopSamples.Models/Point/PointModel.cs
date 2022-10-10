f100c077b9b0bd88f30989041ffd42a3f38922e6
Donat <donat_kolzenov@mail.ru>
2022-10-11 01:45:37 +0700

    add: ctor with x/y parameters

--- samples/DesktopSamples.Models/Point/PointModel.cs
+++ samples/DesktopSamples.Models/Point/PointModel.cs
@@ -2,6 +2,12 @@
 {
     public class PointModel
     {
+        public PointModel(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
         public double X { get; set; }
 
         public double Y { get; set; }
