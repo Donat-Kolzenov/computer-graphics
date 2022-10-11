5a9e5950627b1805dbfb442044c447a853f1ad02
Donat <donat_kolzenov@mail.ru>
2022-10-11 14:19:39 +0700

    update: main window view model

--- samples/KochSnowflake/ViewModels/MainWindowViewModel.cs
+++ samples/KochSnowflake/ViewModels/MainWindowViewModel.cs
@@ -8,19 +8,10 @@
 
     public class MainWindowViewModel : BindableBase
     {
-        private ContentControl _drawingCanvas;
-
-        public DelegateCommand DrawTriangleCommand { get; set; } = null!;
+        public DelegateCommand DrawKochSnowflakeCommand { get; set; } = null!;
 
         public MainWindowViewModel()
         {
-            DrawTriangleCommand = new DelegateCommand(DrawTriangle);
-        }
-
-        private void DrawTriangle()
-        {
-            var triangle = new Models.Triangle(_drawingCanvas, Colors.Blue);
-            triangle.Draw();
         }
     }
 }
