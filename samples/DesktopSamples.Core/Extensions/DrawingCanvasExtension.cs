45a1de72de7bfeba0a87d86e210fb6761745728f
Donat <donat_kolzenov@mail.ru>
2022-10-12 13:32:06 +0700

    add: drawing canvas extension

---
+++ samples/DesktopSamples.Core/Extensions/DrawingCanvasExtension.cs
@@ -0,0 +1,28 @@
+﻿namespace DesktopSamples.Core.Extensions
+{
+    using Avalonia;
+    using Avalonia.Media;
+    using Avalonia.Controls;
+    using Avalonia.Controls.Shapes;
+
+    public static class DrawingCanvasExtension
+    {
+        public static void DrawLine(
+            this Canvas canvas,
+            Point startPoint,
+            Point endPoint,
+            IPen pen)
+        {
+            var line = new Line()
+            {
+                StartPoint = startPoint,
+                EndPoint = endPoint,
+                Stroke = pen.Brush,
+                StrokeThickness = pen.Thickness,
+                StrokeLineCap = pen.LineCap,
+                StrokeJoin = pen.LineJoin
+            };
+            canvas.Children.Add(line);
+        }
+    }
+}
