6a8a8a1f8ed6e4303af4c4b7a3b82bfea2582361
Donat <donat_kolzenov@mail.ru>
2022-10-12 10:53:28 +0700

    add: CustomShape project

---
+++ samples/CustomShape/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace CustomShape
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
